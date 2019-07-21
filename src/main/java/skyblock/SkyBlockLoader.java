1c36f3f5f4570478722ba01306088ca4832a410b
Simon Wanner <skyrising@pvpctutorials.de>
2019-07-21 23:14:36 +0200

    Fix fabric loader loading QuickCarpetModule class before us

--- src/main/java/skyblock/SkyBlockLoader.java
+++ src/main/java/skyblock/SkyBlockLoader.java
@@ -3,13 +3,16 @@ package skyblock;
 import net.fabricmc.api.ModInitializer;
 import quickcarpet.QuickCarpet;
 
+import java.lang.reflect.Method;
+
 public class SkyBlockLoader implements ModInitializer {
     @Override
     public void onInitialize() {
         try {
-            Class.forName("quickcarpet.module.QuickCarpetModule");
-            QuickCarpet.getInstance().registerModule(new SkyBlockModule());
-        } catch (ClassNotFoundException e) {
+            Class<?> qcModuleClass = Class.forName("quickcarpet.module.QuickCarpetModule");
+            Method register = QuickCarpet.class.getDeclaredMethod("registerModule", qcModuleClass);
+            register.invoke(QuickCarpet.getInstance(), new SkyBlockModule());
+        } catch (ReflectiveOperationException e) {
             throw new IllegalStateException("QuickCarpet module system not found. Requires at least QuickCarpet version 1.12.0");
         }
     }
